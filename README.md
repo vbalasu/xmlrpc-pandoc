641e509c16e0df65d1fdc425533ec1d7c72d9820
vbalasubramaniam <vbalasubramaniam@trifacta.com>
2019-04-09 06:08:39 -0700

    Changes

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ One Paragraph of project description goes here
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
 
+![image](https://d301sr5gafysq2.cloudfront.net/frontbucket/pipelines-promotion-icon.acb4e241eab5.png)
+
 ### Prerequisites
 
 What things you need to install the software and how to install them
--- l3_xmlrpc_client.py
+++ l3_xmlrpc_client.py
@@ -1,7 +1,9 @@
 import xmlrpc.client
 import sys, base64
-#s = xmlrpc.client.ServerProxy('http://54.209.175.70:8001', allow_none=True, use_builtin_types=True)
-s = xmlrpc.client.ServerProxy('http://127.0.0.1:8001', allow_none=True, use_builtin_types=True)
-(out, err) = s.run_pandoc('markdown', 'docx', base64.encodebytes(b'# This is some markdown in bytes'))
+s = xmlrpc.client.ServerProxy('http://54.209.175.70:8001', allow_none=True, use_builtin_types=True)
+#s = xmlrpc.client.ServerProxy('http://127.0.0.1:8001', allow_none=True, use_builtin_types=True)
+with open("README.md", "rb") as f:
+    markdown = f.read()
+(out, err) = s.run_pandoc('markdown', 'docx', base64.encodebytes(markdown))
 sys.stdout.buffer.write(out)
 #USAGE: python l3_xmlrpc_client.py
